3a7d9eee2e90e04618397faecf0c157fba0e6dbd
Oscar <o.velillaa@gmail.com>
2022-09-15 22:35:11 +0200

    updates

--- application/app.php
+++ application/app.php
@@ -3,5 +3,13 @@
 require '../vendor/autoload.php';
 require 'scripts/helpers.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+use Dotenv\Dotenv;
+
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
